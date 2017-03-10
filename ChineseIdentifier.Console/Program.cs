dd45be50a5d36e5331f1d0de95d8470e85ca721b
Thomas Woolley <thomas.woolley92@outlook.com>
2017-03-10 22:14:06 +0000

    Added console runner - setup example build event

---
+++ ChineseIdentifier.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChineseIdentifier.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var resxChecker = new ResxChecker();
+            var chineseType = (ChineseType)Enum.Parse(typeof(ChineseType), args[0]);
+            var resxFile = args[1];
+
+            var result = resxChecker.IsOfType(chineseType, resxFile);
+
+            if (result.WasMatch)
+            {
+                System.Console.WriteLine("File check complete - OK");
+            }
+            else
+            {
+                System.Console.WriteLine($"File check complete - {result.Message}");
+            }
+        }
+    }
+}
